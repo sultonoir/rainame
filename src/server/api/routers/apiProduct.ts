d3dd3593ac42b0786d9c8ab70c2ae973b9ab1afb
Sulton <sultonalidrus5@gmail.com>
2024-01-08 21:56:39 +0700

    Revert "style : remove the console in apiProduct"
    This reverts commit 58e7774fe55e6e1f6815e2693ed312fcb2a48776.

--- src/server/api/routers/apiProduct.ts
+++ src/server/api/routers/apiProduct.ts
@@ -338,6 +338,7 @@ export const apiProduct = createTRPCRouter({
       }
 
       if (input.promo) {
+        console.log(input.promo);
         const name = decodeURIComponent(input.promo);
         const promo = await ctx.db.promo.findUnique({
           where: {
