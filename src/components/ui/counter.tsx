6c94b613d47e8d689c33daf15ad5c966ae364dba
Sulton <sultonalidrus5@gmail.com>
2024-05-13 13:52:46 +0700

    style : change style counter button

--- src/components/ui/counter.tsx
+++ src/components/ui/counter.tsx
@@ -15,21 +15,21 @@ const Counter = ({ id, price }: Props) => {
   const amount = findcart ? findcart : 1;
 
   return (
-    <div className="flex items-center gap-2 rounded-full">
+    <div className="flex items-center gap-2 rounded-lg border p-0.5">
       <Button
         size="icon"
         variant="outline"
-        className="size-6 rounded-full border border-muted-foreground hover:border-2"
+        className="size-6 border-none hover:bg-transparent"
         disabled={amount <= 1}
         onClick={() => decrement({ id, amount: 1, price })}
       >
         <Minus />
       </Button>
-      <span>{amount}</span>
+      <span className="size-6 flex-shrink-0 text-center">{amount}</span>
       <Button
         size="icon"
         variant="outline"
-        className="size-6 rounded-full border border-muted-foreground hover:border-2"
+        className="size-6 border-none hover:bg-transparent"
         onClick={() => increment({ id, amount: 1, price })}
       >
         <Plus />
