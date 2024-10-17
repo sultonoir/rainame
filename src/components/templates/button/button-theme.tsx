98e4bbb405d05a59c60ff99ef1d4995bb03d79cf
Sulton <sultonalidrus5@gmail.com>
2024-10-18 06:08:06 +0700

    fix : suspense hyration warning

--- src/components/templates/button/button-theme.tsx
+++ src/components/templates/button/button-theme.tsx
@@ -2,11 +2,18 @@
 
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 export const ButtonTheme = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   const handleClick = () => {
     if (theme === "dark") {
       setTheme("light");
