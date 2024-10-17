0fa4fcd684bce82989476dcfbfec9b2602b2c71b
Sulton <sultonalidrus5@gmail.com>
2024-10-18 02:05:59 +0700

    ref : button oauth

--- src/components/templates/button/button-oauth.tsx
+++ src/components/templates/button/button-oauth.tsx
@@ -1,15 +1,12 @@
 "use client";
 import React from "react";
 import { ButtonLoading } from "./button-loading";
-import { ChromeIcon, GithubIcon } from "lucide-react";
+import { ChromeIcon } from "lucide-react";
 import { signIn } from "@/lib/auth-client";
 
 export default function ButtonOauth() {
   return (
     <div className="flex flex-col space-y-2">
-      <ButtonLoading variant="outline">
-        <GithubIcon />
-      </ButtonLoading>
       <ButtonLoading
         variant="outline"
         onClick={async () => {
