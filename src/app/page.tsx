1eef49e91e9f16f97b0d2b14482eeb95d7e9a962
Sulton <sultonalidrus5@gmail.com>
2026-05-27 06:00:19 +0900

    feat(ui) : add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ProductsApi from "./product-api";
 import { Suspense } from "react";
+import { CalendarBasic } from "./cal";
 
 function LoadingSkeleton() {
   return (
@@ -20,6 +21,9 @@ export default async function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Suspense>
+          <CalendarBasic />
+        </Suspense>
         <div className="flex gap-10 w-full">
           <Suspense fallback={<LoadingSkeleton />}>
             <ProductsApi />
